7fb5da2850158e8f32a78b6b7dadaea65c56dde2
kevin garcia <kevin.garcia.an.098@gmail.com>
2023-11-24 00:07:12 -0300

    chore:agregamos una nueva funcion

--- clase_12eventos/scrips.js
+++
@@ -1,6 +0,0 @@
-let boton= document.querySelector("button")
-boton.addEventListener("click",function () {
-    console.log("click funcionando")
-    boton.textContent="el boton funciona"
-    boton.style.backgroundColor="blue"
-})
